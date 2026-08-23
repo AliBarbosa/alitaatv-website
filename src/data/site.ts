914d2b367bfe3eec9d78f55b061d35a9b64d1f00
AlitaaTV <alitaatv@users.noreply.github.com>
2026-08-23 02:15:59 +0200

    Refine homepage content and setup section

--- src/data/site.ts
+++ src/data/site.ts
@@ -3,15 +3,22 @@ export const site = {
   name: 'AlitaaTV', domain: 'www.alitaatv.com', email: 'contacto@alitaatv.com',
   twitch: 'https://www.twitch.tv/alitaatv',
   links: [
-    { name: 'Twitch', detail: 'Directos y comunidad', url: 'https://www.twitch.tv/alitaatv', icon: '▶', tone: 'pink' },
-    { name: 'Patreon', detail: 'Apoya el proyecto', url: 'https://www.patreon.com/cw/AlitaaTV', icon: '★', tone: 'orange' },
-    { name: 'Discord', detail: 'Únete al chat', url: 'https://discord.gg/9BDYrWpPsM', icon: '✦', tone: 'violet' },
-    { name: 'Instagram', detail: 'Clips y novedades', url: 'https://www.instagram.com/alitaatv', icon: '◌', tone: 'blue' },
+    { name: 'Discord', url: 'https://discord.gg/9BDYrWpPsM', icon: 'discord' },
+    { name: 'Patreon', url: 'https://www.patreon.com/cw/AlitaaTV', icon: 'patreon' },
+    { name: 'Instagram', url: 'https://www.instagram.com/alitaatv', icon: 'instagram' },
+    { name: 'Facebook', url: 'https://www.facebook.com/people/Alitaatv/61582213929665/', icon: 'facebook' },
+    { name: 'YouTube', url: 'https://www.youtube.com/@alitaatv', icon: 'youtube' },
+    { name: 'TikTok', url: 'https://www.tiktok.com/@alitaatv', icon: 'tiktok' },
   ],
-  semalita: [
-    { title: 'La Semalita #1', url: 'https://www.instagram.com/reel/DcFK8xXIbGM/' },
-    { title: 'La Semalita #2', url: 'https://www.instagram.com/reel/DcWLyuWCPgn/' },
+  featured: [
+    { title: 'YouTube Short 01', platform: 'YouTube', embed: 'https://www.youtube.com/embed/O06PDbXlJDY', url: 'https://www.youtube.com/shorts/O06PDbXlJDY' },
+    { title: 'YouTube Short 02', platform: 'YouTube', embed: 'https://www.youtube.com/embed/ZvfwUGueFdg', url: 'https://www.youtube.com/shorts/ZvfwUGueFdg' },
+    { title: 'YouTube Short', platform: 'YouTube', embed: 'https://www.youtube.com/embed/Ku40mPi8uf4', url: 'https://www.youtube.com/shorts/Ku40mPi8uf4' },
   ],
+  support: {
+    patreon: 'https://www.patreon.com/cw/AlitaaTV',
+    tip: 'https://streamelements.com/alitaatv/tip',
+  },
   socials: [
     { name: 'YouTube', url: 'https://www.youtube.com/@alitaatv' }, { name: 'TikTok', url: 'https://www.tiktok.com/@alitaatv' },
     { name: 'Instagram', url: 'https://www.instagram.com/alitaatv' }, { name: 'Facebook', url: 'https://www.facebook.com/people/Alitaatv/61582213929665/' },
